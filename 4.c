bc0e258a3af227eaecb037a42c1de969216eec63
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-10-09 20:41:45 +0800

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int year,a;
+    scanf("%d",&year);
+    if(year%400==0)
+        a=1;
+    else
+    {
+        if(year%4==0&&year%100!=0)
+            a=1;
+        else
+            a=0;
+    }
+    if(a==1)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    return 0;
+}
