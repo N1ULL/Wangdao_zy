509ec5cf55c3b8d93ca97fd8ccee106810c082c6
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-11-30 13:41:43 +0800

    Add files via upload

---
+++ 13_2.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int climbStairs(int n) {
+    int p = 0, q = 0, r = 1;
+    for (int i = 1; i <= n; ++i) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d",climbStairs(n));
+    return 0;
+}
