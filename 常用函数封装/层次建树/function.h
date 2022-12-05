b8bf51b568ab236924348c158e0b2b4847d3dcb3
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-12-05 15:25:48 +0800

    Create function.h

---
+++ 常用函数封装/层次建树/function.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef char BiElemType;
+typedef struct BiTNode
+{
+	BiElemType c;
+	struct BiTNode* lchild;
+	struct BiTNode* rchild;
+}BiTNode,*BiTree;
+
+//tag结构体是辅助队列使用的
+typedef struct tag
+{
+	BiTree p;//树的某一个结点的地址值
+	struct tag* pnext;
+}tag_t,*ptag_t;
